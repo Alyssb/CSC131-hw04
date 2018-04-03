ca551e45d152aa205960d1d5ac76ce85a4284a62
Alyssb <ajs41@missouristate.edu>
2018-04-03 13:30:57 -0500

    few edits and reformatting included

--- hw04.cpp
+++ hw04.cpp
@@ -13,7 +13,7 @@
 /**
  * takes an inputted time and calculates the military format of said time
  * relies on time being formatted correctly (see next line)
- * @param st the inputted time (format hh:mm xm)
+ * @param st the inputted time (format hh:mm xm or h:mm xm)
  * @return military time is returned (format hhmm)
  */
 std::string convertToMilitaryTime(std::string);
@@ -26,9 +26,9 @@ std::string convertToMilitaryTime(std::string);
  */
 int main(int argc, char* argv[])
 {
-    std::cout << "what time is it?? ";
+    std::cout << "what time is it, including am/pm?? ";
     std::string ogTime{};
-    std::getline(std::cin, ogTime);
+    getline(std::cin, ogTime);
     std::cout << "so it's "
               << ogTime
               << "?"
@@ -46,35 +46,32 @@ std::string convertToMilitaryTime(std::string st)
     int hour{};
     std::string miltime{};
 
-    hour = std::stoi(st.substr(0,st.find(':')));
-    std::string min = st.substr(st.find(':')+1,st.find(' ')-2);
+    hour = std::stoi(st.substr(0, st.find(':')));
+    std::string min = st.substr(st.find(':')+1, st.find(' ')-2);
 
     //value of min changed based on length of st. This makes all min the same
     if (min.at(min.length()-1)==' ')
-        min = min.substr(0,min.length()-1);
+        min = min.substr(0, min.length()-1);
 
     //calculates military time for 12:XX pm and 12:XX am
-    if (hour == 12)
-    {
-        if (st.at(6) == 'p' || st.at(6) == 'P')
-            miltime = std::to_string(hour) + min;
+    if (hour==12) {
+        if (st.at(6)=='p' || st.at(6)=='P')
+            miltime = std::to_string(hour)+min;
         else
-            miltime = "00" + min;
+            miltime = "00"+min;
     }
-    //calculates military time for pm times
-    else if (st.find('p') != std::string::npos || st.find('P') != std::string::npos)
-    {
-        hour = hour + 12;
+        //calculates military time for pm times
+    else if (st.find('p')!=std::string::npos || st.find('P')!=std::string::npos) {
+        hour = hour+12;
         std::cout << hour << std::endl;
-        miltime = std::to_string(hour) + min;
+        miltime = std::to_string(hour)+min;
     }
-    //calculates military time for am times
-    else
-    {
-        if (hour < 10)
-            miltime = "0" + std::to_string(hour) + min;
+        //calculates military time for am times
+    else {
+        if (hour<10)
+            miltime = "0"+std::to_string(hour)+min;
         else
-            miltime = std::to_string(hour) + min;
+            miltime = std::to_string(hour)+min;
     }
 
     return miltime;
